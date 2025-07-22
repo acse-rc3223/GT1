e4378f9bb95cdfe3fdb3a586db3fac811f366a92
Rory <Rory@ANNABELLE-LAP2.communityfibre.co.uk>
2025-07-22 11:28:27 +0100

    Initial commit.

---
+++ GT1/src/co/uk/diegesis/Rory/CorneliusSmith/GT1/GT1Constants.java
@@ -0,0 +1,5 @@
+package co.uk.diegesis.Rory.CorneliusSmith.GT1;
+
+public class GT1Constants {
+
+}
---
+++ GT1/src/co/uk/diegesis/Rory/CorneliusSmith/GT1/GT1Main.java
@@ -0,0 +1,12 @@
+package co.uk.diegesis.Rory.CorneliusSmith.GT1;
+
+public class GT1Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.exit(0);
+
+	}
+
+}
