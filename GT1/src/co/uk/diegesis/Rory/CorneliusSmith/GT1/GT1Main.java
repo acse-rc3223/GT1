da455a758e7dec2dd49e117da8caf3777f0d10fd
Rory <Rory@ANNABELLE-LAP2.communityfibre.co.uk>
2025-07-22 12:29:10 +0100

    Initial thread behaviour - not exiting correctly.

--- GT1/src/co/uk/diegesis/Rory/CorneliusSmith/GT1/GT1Main.java
+++ GT1/src/co/uk/diegesis/Rory/CorneliusSmith/GT1/GT1Main.java
@@ -2,8 +2,16 @@ package co.uk.diegesis.Rory.CorneliusSmith.GT1;
 
 public class GT1Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
+		GT1Thread gt1Thread = new GT1Thread();
+		gt1Thread.start();
+		
+		// let the thread run for some time
+		Thread.sleep(5000);
+		
+		// call the setter method for the exit
+		gt1Thread.setExit();
 		
 		System.exit(0);
 
--- GT1/src/co/uk/diegesis/Rory/CorneliusSmith/GT1/GT1Thread.java
+++ GT1/src/co/uk/diegesis/Rory/CorneliusSmith/GT1/GT1Thread.java
@@ -1,5 +1,32 @@
 package co.uk.diegesis.Rory.CorneliusSmith.GT1;
 
-public class GT1Thread {
+// extends Thread to inherit behaviour
+public class GT1Thread extends Thread{
+	private boolean isExiting = false;
+	private long threadSleepTime = 1000;
+	
+//	run() method
+	public void run() {
+		System.out.println(getName() + " thread has started.");
+		
+		while (!isExiting){
+			System.out.println(getName() + " thread is sleeping...");
+            try {
+				Thread.sleep(threadSleepTime);
+			} 
+            
+            catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		System.out.println(getName() + " thread is exiting.");
+		
+	}
 
+//	setExit() method to set isExisting to true
+	public void setExit() {
+		isExiting = true;
+	}
+	
 }
