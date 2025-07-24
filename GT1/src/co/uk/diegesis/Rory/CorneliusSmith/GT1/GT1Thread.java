e2b76bd8020faceec5d4c7c1bbc3b4fcc3bd88e8
roryj <roryj@ANNABELLE-LAP>
2025-07-24 13:38:26 +0100

    Volatility, thread overrun catch, commenting restructured.

--- GT1/src/co/uk/diegesis/Rory/CorneliusSmith/GT1/GT1Thread.java
+++ GT1/src/co/uk/diegesis/Rory/CorneliusSmith/GT1/GT1Thread.java
@@ -2,30 +2,49 @@ package co.uk.diegesis.Rory.CorneliusSmith.GT1;
 
 // extends Thread to inherit behaviour
 public class GT1Thread extends Thread{
-	private boolean isExiting = false;
 	
-//	run() method
+	/* volatile keyword ensures reading and writing 
+	 * threads have the same value*/
+	private volatile boolean isExiting = false;
+	
+	// record when we start to allow thread termination if setExit fails
+	private final long startTime = System.currentTimeMillis();
+	
+	// run() method overriding the Thread definition
+	@Override
 	public void run() {
-		System.out.println(getName() + " thread has started.");
+		System.out.println(this.getName() + " thread has started.");
 		
 		while (!isExiting){
-			System.out.println(getName() + " thread is sleeping...");
+			
+			if (System.currentTimeMillis() - startTime > GT1Constants.MAX_THREAD_RUNTIME) {
+				System.out.println(this.getName() + " reached max runtime, exiting.");
+				// we want to exit the loop and terminate the thread if its run too long and setExit has failed
+				break;
+			}
+			
+			System.out.println(this.getName() + " thread is sleeping...");
             try {
-				Thread.sleep(GT1Constants.THREAD_SLEEP_TIME);
-			} 
+            	Thread.sleep(GT1Constants.THREAD_SLEEP_TIME);
+            }
             
             catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				System.out.println(this.getName() + " thread has failed...");
+				Thread.currentThread().interrupt();
+				// we want to exit the loop and terminate the thread avoiding it re-awakening
+				break;
 			}
 		}
-		System.out.println(getName() + " thread is exiting.");
 		
+		return;
 	}
 
-//	setExit() method to set isExisting to true
-	public void setExit() {
-		isExiting = true;
+	// setExit() method to set isExisting to true
+	public void setExit(boolean shouldExit) {
+		// set as parameter instead of a hard code boolean
+		this.isExiting = shouldExit;
+		System.out.println(this.getName() + " thread is exiting.");
+		return;
 	}
 	
 }
