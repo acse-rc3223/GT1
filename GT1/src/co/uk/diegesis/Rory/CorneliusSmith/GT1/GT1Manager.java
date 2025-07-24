ccace6cc60daf3f52338b82c44693b166a048f27
roryj <roryj@ANNABELLE-LAP>
2025-07-24 13:37:36 +0100

    Array list instead of list and parameterised values and constants used.

--- GT1/src/co/uk/diegesis/Rory/CorneliusSmith/GT1/GT1Manager.java
+++ GT1/src/co/uk/diegesis/Rory/CorneliusSmith/GT1/GT1Manager.java
@@ -1,5 +1,8 @@
 package co.uk.diegesis.Rory.CorneliusSmith.GT1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GT1Manager {
 	
 	public void runOneThread() throws InterruptedException{
@@ -11,44 +14,51 @@ public class GT1Manager {
 		gt1Thread.start();
 		
 		// let the thread run for some time, by sleeping the main thread
-		//Thread.sleep(5000);
+		Thread.sleep(GT1Constants.MANAGER_WAIT_TIME);
 		
 		// call the setter method for the exit
-		gt1Thread.setExit();
+		gt1Thread.setExit(true);
 		
 		// need to allow the thread to finish before System.exit
 		gt1Thread.join();
+		
+		return;
 	}
 	
 	public void runManyThreads(int N_THREADS) throws InterruptedException{
 		
-		// create an array of the threads
-		GT1Thread[] arrGT1Threads = new GT1Thread[N_THREADS];
+		// create an arrayList of the threads (changed from basic array)
+		List<GT1Thread> threads = new ArrayList<>(N_THREADS);
 		
 		// create a for loop to initialise each thread and start it
 		for (int i = 0; i < N_THREADS; i++) {
-			
 			// create each instance of the threads at index i
-			arrGT1Threads[i] = new GT1Thread();
-			
+			GT1Thread gt1Thread = new GT1Thread();
+			// add the new thread to the arrayList
+			threads.add(gt1Thread);
+		}
+		
+		for (GT1Thread gt1Thread : threads) {
 			// start each array object as we loop through
-			arrGT1Threads[i].start();
+			gt1Thread.start();
 		}
 			
 		// sleep the main thread outside of initialisation for parallel thread running?
-		Thread.sleep(GT1Constants.THREAD_SLEEP_TIME);
+		Thread.sleep(GT1Constants.MANAGER_WAIT_TIME);
 			
 		// create a for loop to exit each thread
-		for (int i = 0; i < N_THREADS; i++) {
+		for (GT1Thread gt1Thread : threads) {
 			// once the thread has woken up we call the setExit() method
-			arrGT1Threads[i].setExit();
+			gt1Thread.setExit(true);
 		}
 		
-		// create a for loop to kill all the threads so that we can flush the console
-		for (int i = 0; i < N_THREADS; i++) {
+		// create a for loop to wait for all threads to die
+		for (GT1Thread gt1Thread : threads) {
 			// need to allow all the threads to finish and die before System.exit
-			arrGT1Threads[i].join();
+			gt1Thread.join();
 		}
+		
+		return;
 	}
 	
 
