dd301e2101d45747c74f55d91d8199ab865a78b2
roryj <roryj@ANNABELLE-LAP>
2025-07-24 13:35:48 +0100

    Added wait times for manager and max thread wait time.

--- GT1/src/co/uk/diegesis/Rory/CorneliusSmith/GT1/GT1Constants.java
+++ GT1/src/co/uk/diegesis/Rory/CorneliusSmith/GT1/GT1Constants.java
@@ -1,6 +1,8 @@
 package co.uk.diegesis.Rory.CorneliusSmith.GT1;
 
 public class GT1Constants {
-	public static final long THREAD_SLEEP_TIME = 1000;
+	public static final int THREAD_SLEEP_TIME   = 1000;
+	public static final long MANAGER_WAIT_TIME  = 5000;
+	public static final long MAX_THREAD_RUNTIME = 10_000;
 
 }
