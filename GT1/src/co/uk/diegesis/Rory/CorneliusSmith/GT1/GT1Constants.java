ee8e93e02d29691de0667d6c8dbf908c9f8c916a
roryj <roryj@ANNABELLE-LAP.communityfibre.co.uk>
2025-07-25 14:02:39 +0100

    Commented out max thread runtime.

--- GT1/src/co/uk/diegesis/Rory/CorneliusSmith/GT1/GT1Constants.java
+++ GT1/src/co/uk/diegesis/Rory/CorneliusSmith/GT1/GT1Constants.java
@@ -4,6 +4,6 @@ public class GT1Constants {
 	public static final int N_THREADS           = 15;
 	public static final int THREAD_SLEEP_TIME   = 1000;
 	public static final long MANAGER_WAIT_TIME  = 5000;
-	public static final long MAX_THREAD_RUNTIME = 10_000;
+//	public static final long MAX_THREAD_RUNTIME = 10_000;
 
 }
