b4961be1304b8d2a8b1b80feded144281b852db8
roryj <roryj@ANNABELLE-LAP.communityfibre.co.uk>
2025-07-25 09:25:54 +0100

    Getter method to access myProtectedInt

--- GT1/src/co/uk/diegesis/Rory/CorneliusSmith/GT1/GT1SynchronisedData.java
+++ GT1/src/co/uk/diegesis/Rory/CorneliusSmith/GT1/GT1SynchronisedData.java
@@ -1,17 +1,22 @@
 package co.uk.diegesis.Rory.CorneliusSmith.GT1;
 
 public class GT1SynchronisedData {
-	public volatile long myprotectedInt = 0;
+	private long myprotectedInt = 0;
 	
 	public synchronized void incrementMyValue(){
 		// simply add 1
-		myprotectedInt ++;
+		myprotectedInt++;
 		return;
 	}
 	public synchronized void decrementMyValue(){
 		// simply subtract 1
-		myprotectedInt --;
+		myprotectedInt--;
 		return;
+	}
+	
+	// create a getter method so that threads can see myprotectedInt
+	public synchronized long getMyValue() {
+		return myprotectedInt;
 	}
 
 }
