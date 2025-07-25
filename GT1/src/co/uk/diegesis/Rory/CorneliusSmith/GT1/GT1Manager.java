d4735246298a61da47a615721a723a93368964da
roryj <roryj@ANNABELLE-LAP.communityfibre.co.uk>
2025-07-25 09:25:35 +0100

    N_Threads, isAlive check, skeleton method for runThreadsData

--- GT1/src/co/uk/diegesis/Rory/CorneliusSmith/GT1/GT1Manager.java
+++ GT1/src/co/uk/diegesis/Rory/CorneliusSmith/GT1/GT1Manager.java
@@ -25,13 +25,13 @@ public class GT1Manager {
 		return;
 	}
 	
-	public void runManyThreads(int N_THREADS) throws InterruptedException{
+	public void runManyThreads() throws InterruptedException{
 		
 		// create an arrayList of the threads (changed from basic array)
-		List<GT1Thread> threads = new ArrayList<>(N_THREADS);
+		List<GT1Thread> threads = new ArrayList<>(GT1Constants.N_THREADS);
 		
 		// create a for loop to initialise each thread and start it
-		for (int i = 0; i < N_THREADS; i++) {
+		for (int i = 0; i < GT1Constants.N_THREADS; i++) {
 			// create each instance of the threads at index i
 			GT1Thread gt1Thread = new GT1Thread();
 			// add the new thread to the arrayList
@@ -55,7 +55,49 @@ public class GT1Manager {
 		// create a for loop to wait for all threads to die
 		for (GT1Thread gt1Thread : threads) {
 			// need to allow all the threads to finish and die before System.exit
-			gt1Thread.join();
+			// first check if the thread is alive
+			if (gt1Thread.isAlive()) {
+				gt1Thread.join();
+			}
+		}
+		return;
+	}
+		
+	
+	public void runThreadsData() throws InterruptedException{
+		
+		// create an arrayList of the threads (changed from basic array)
+		List<GT1Thread> threads = new ArrayList<>(GT1Constants.N_THREADS);
+		
+		// create a for loop to initialise each thread and start it
+		for (int i = 0; i < GT1Constants.N_THREADS; i++) {
+			// create each instance of the threads at index i
+			GT1Thread gt1Thread = new GT1Thread();
+			// add the new thread to the arrayList
+			threads.add(gt1Thread);
+		}
+		
+		for (GT1Thread gt1Thread : threads) {
+			// start each array object as we loop through
+			gt1Thread.start();
+		}
+			
+		// sleep the main thread outside of initialisation for parallel thread running?
+		Thread.sleep(GT1Constants.MANAGER_WAIT_TIME);
+			
+		// create a for loop to exit each thread
+		for (GT1Thread gt1Thread : threads) {
+			// once the thread has woken up we call the setExit() method
+			gt1Thread.setExit(true);
+		}
+		
+		// create a for loop to wait for all threads to die
+		for (GT1Thread gt1Thread : threads) {
+			// need to allow all the threads to finish and die before System.exit
+			// first check if the thread is alive
+			if (gt1Thread.isAlive()) {
+				gt1Thread.join();
+			}
 		}
 		
 		return;
