c7fe8522d6f0f333e7914b305ff55bbd53b55848
roryj <roryj@ANNABELLE-LAP.communityfibre.co.uk>
2025-07-25 14:10:33 +0100

    Methods all commented out for use.

--- GT1/src/co/uk/diegesis/Rory/CorneliusSmith/GT1/GT1Main.java
+++ GT1/src/co/uk/diegesis/Rory/CorneliusSmith/GT1/GT1Main.java
@@ -2,12 +2,14 @@ package co.uk.diegesis.Rory.CorneliusSmith.GT1;
 
 public class GT1Main {
 
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) {
 		// create an instance of the manager on the main thread
 		GT1Manager gt1Manager = new GT1Manager();
 		
+		// Uncomment chosen method below to run
+		
 //		gt1Manager.runOneThread();
-		gt1Manager.runManyThreads();
+//		gt1Manager.runManyThreads();
 //		gt1Manager.runThreadsData();
 		
 		System.exit(0);
